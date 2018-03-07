18a101035915811291801a0c27d258c939073ef6
plutto <plutto_sun@hotmail.com>
2018-03-08 07:04:40 +1100

    question5 and 6

---
+++ question5/question5.sql
@@ -0,0 +1,13 @@
+﻿--5.1 
+----total price of all orders
+SELECT SUM(Price) FROM Orders
+----products sold per order
+SELECT AVG(oi.Quantity)
+FROM Orders o
+INNER JOIN OrderItems oi ON o.id=oi.Order_Id
+---- largest in terms of unit price?
+Sorry, dont understand the question
+
+--5.2 Optimize
+1. REMOVE 'price' FROM Orders TABLE AS it can be calculated BY OrderITems TABLE
+ 
---
+++ question6/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+
+    #region 6.1
+    class ICoreService
+    {
+
+    }
+    class UserICoreService
+    {
+        public ICoreService CoreService
+        {
+            get;
+            protected set;
+        }
+    }
+    #endregion
+
+    #region 6.2
+    class Contact
+    {
+
+    }
+    class UseContact
+    {
+        private Contact _FirstEmergencyContact;
+        public Contact FirstEmergencyContact
+        {
+            get { return _FirstEmergencyContact; }
+            set { SetProperty( _FirstEmergencyContact, value); }
+        }
+        private Contact _SecondEmergencyContact;
+        public Contact SecondEmergencyContact
+        {
+            get { return _SecondEmergencyContact; }
+            set { SetProperty( _FirstEmergencyContact, value); }
+        }
+
+       Contact SetProperty(Contact contact, object value) {
+            return contact;
+        }
+    }
+    
+    #endregion
+
+
+}
